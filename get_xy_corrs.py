3c8427b92a396f6645df8e0a3f849db206f61a44
jovonden <jost.von.den.driesch@cern.ch>
2024-10-16 10:24:59 +0200

    improved readability

--- get_xy_corrs.py
+++ get_xy_corrs.py
@@ -1,6 +1,5 @@
 # general packages
 import ROOT
-import os
 import json
 import logging
 
@@ -46,16 +45,14 @@ def main():
     if args.prep:
         get_files_from_das(path_dict['datasets'])
 
-    # load files
-    with open(path_dict['files'], 'r') as f:
-        allfiles = json.load(f)
-
     # step 1: make flat ntuples with necessary information
     if args.snapshot:
         make_snapshot(
-            allfiles,
-            path_dict['golden_json'], path_dict['pu_json'],
-            mets, list(hbins['pileup'].keys()),
+            path_dict['files'],
+            path_dict['golden_json'], 
+            path_dict['pu_json'],
+            mets, 
+            list(hbins['pileup'].keys()),
             path_dict['snap_dir'],
             args.jobs,
             args.condor, path_dict['condor_dir'],
@@ -66,19 +63,36 @@ def main():
 
     # step 2: make 2d histograms met xy vs pileup
     if args.hists:
-        make_hists(path_dict['snap_dir'], path_dict['hist_dir'], hbins, args.jobs)
+        make_hists(
+            path_dict['snap_dir'],
+            path_dict['hist_dir'],
+            hbins,
+            args.jobs
+        )
 
     # step 3: fit linear functions to 2d histograms
     if args.corr:
-        get_corrections(path_dict['hist_dir'], hbins, path_dict['corr_dir'], path_dict['plot_dir'], mets, lumilabels)
+        get_corrections(
+            path_dict['hist_dir'],
+            hbins,
+            path_dict['corr_dir'],
+            path_dict['plot_dir'],
+            mets,
+            lumilabels
+        )
 
     # closure
     if args.validate:
-        make_validation_plots(path_dict['snap_dir'], path_dict['plot_dir'], path_dict['corr_dir'], hbins, axislabels, lumilabels)
+        make_validation_plots(
+            path_dict['snap_dir'],
+            path_dict['plot_dir'],
+            path_dict['corr_dir'],
+            hbins,
+            axislabels,
+            lumilabels
+        )
 
 
 if __name__=='__main__':
     main()
-# TODO: discuss whether the choice of no filter introduces biases.
-# TODO: introduce pileup correction?
 
