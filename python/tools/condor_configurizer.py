94aaf6c4690e0f818144e4e98d3deb84c13b5211
Jost von den Driesch <jostvddriesch@gmail.com>
2024-11-21 00:14:37 +0100

    changed condor submit style to lxplus

--- python/tools/condor_configurizer.py
+++ python/tools/condor_configurizer.py
@@ -8,9 +8,12 @@ def setup_job(condor_dir, dtmc, year):
     # setup condor job script
     path = os.getcwd()
     job_script = f"#!/bin/bash \n"\
+        f"export X509_USER_PROXY=$2 \n"\
+        "voms-proxy-info -all \n"\
+        "voms-proxy-info -all -file $2 \n"\
         f"cd {path} \n"\
         f"source env.sh \n"\
-        f"python get_xy_corrs.py -S --condor $1 --process {dtmc} --year {year}"
+        f"python get_xy_corrs.py -S --condor $1 --process {dtmc} --year {year} --debug"
 
     os.makedirs(f'{condor_dir}{dtmc}/logs/', exist_ok=True)
     with open(f'{condor_dir}{dtmc}/job.sh', 'w') as job:
@@ -19,61 +22,25 @@ def setup_job(condor_dir, dtmc, year):
     return
 
 
-def setup_condor_etp(njobs, condor_dir, dtmc):
+def setup_condor_lxplus(njobs, condor_dir, dtmc, proxy_path):
 
     logger.info("Setting up the submit file.")
 
     # setup condor submit file
     submit_script = f"""executable = ./job.sh
-arguments = $(Process)
-
-# Output/Error/Log files
-Output = logs/job_$(Cluster)_$(Process).out
-Error  = logs/job_$(Cluster)_$(Process).err
-Log    = logs/job_$(Cluster)_$(Process).log
-
-# job requirements
-+RequestWalltime = 3600*2
-RequestCPUs = 1
-RequestMemory = 2000
-request_disk = 5000000
-Requirements = TARGET.ProvidesEKPResources =?= True
-accounting_group = cms.higgs
-Universe = docker
-docker_image = mschnepf/slc7-condocker:latest
-getenv = True
-
-queue {njobs}"""
-    
-    path_submit = f'{condor_dir}{dtmc}/submit.sub'
-    logger.info(f"Saving submit file in {path_submit}.")
-
-    with open(path_submit, 'w') as submit:
-        submit.write(submit_script)
-
-    run_script = f"cd {condor_dir}{dtmc}/ && condor_submit submit.sub && cd ../../../../.."
-
-    logger.info(f"Run script via: {run_script}")    
-    return
-
-
-def setup_condor_lxplus(njobs, condor_dir, dtmc):
-
-    logger.info("Setting up the submit file.")
-
-    # setup condor submit file
-    submit_script = f"""executable = ./job.sh
-arguments = $(Process)
+arguments = $(Process) $(Proxy_path)
 
 # output/error/log files
 output = logs/job_$(Cluster)_$(Process).out
-eror = logs/job_$(Cluster)_$(Process).err
+error = logs/job_$(Cluster)_$(Process).err
 log = logs/job_$(Cluster)_$(Process).log
 
 # job requirements
 universe = vanilla
 +JobFlavour = "espresso"
 RequestCPUs = 1
+
+Proxy_path = {proxy_path}
 
 queue {njobs}"""
     
