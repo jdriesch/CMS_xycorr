f25dcba6f312e3a6f7265a0d9a8f4182939a3884
jovonden <jost.von.den.driesch@cern.ch>
2024-11-20 03:43:16 +0100

    added golden_json on lxplus

--- inputs/config/paths.py
+++ inputs/config/paths.py
@@ -11,14 +11,19 @@ def get_paths(args):
         'corr_dir': f"results/corrections/{add_path}/",
         'hist_dir': f"results/hists/{add_path}/",
         'condor_dir': f"results/condor/{add_path}/",
-        'golden_json': 'inputs/jsons/Run3_2022_2023_Golden.json',
         'pu_json': f'inputs/jsonpog/POG/LUM/{args.year}/puWeights.json.gz',
         'snap_dir': f"/ceph/jdriesch/CMS_xycorr/snapshots/{add_path}/",
+    }
+
+    golden_jsons = {
+        "2022_Summer22": "/eos/user/c/cmsdqm/www/CAF/certification/Collisions22/Cert_Collisions2022_355100_362760_Golden.json"
     }
 
     for key in paths.keys():
         if 'dir' in key:
             os.makedirs(paths[key], exist_ok=True)
+
+    paths['golden_json'] = golden_jsons[args.year]
 
     os.makedirs(paths['nanoAODs'].replace(paths['nanoAODs'].split('/')[-1], ''), exist_ok=True)
 
